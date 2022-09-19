30e8c5987abb855e898966ba151d2c57c4619423
Jesse Warren <jesse.mwarren@yahoo.com>
2022-09-19 11:40:14 -0700

    Add dependency and generation code

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const generateUUID = () => {
+const fs = require('fs');
+const wordListPath = require('word-list');
+
+const generateUUID = (length=4) => {
+  const words = fs.readFileSync(wordListPath, 'utf8').split('\n');
+  let result = "";
   
+  for (let i = 0; i < length; i++) {
+    const rIndex = Math.floor(Math.random() * words.length)
+    result += words[rIndex]
+    if (i < length - 1) {
+      result += "-"
+    }
+  }
+  
+  return result;
 }
 
 export default generateUUID;
